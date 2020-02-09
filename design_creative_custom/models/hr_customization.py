f9f1f9954cfc993a7ac87b2865fb23bcbd72e89b
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-02-09 18:42:25 +0500

    c

--- design_creative_custom/models/hr_customization.py
+++ design_creative_custom/models/hr_customization.py
@@ -24,26 +24,21 @@ class inherithremploye(models.Model):
     accomodation = fields.Selection([('YES', 'YES'), ('NO', 'NO')], string='Acomodation', )
 
 
-class inheritcontract(models.Model):
-    _inherit = 'hr.contract'
-
-
-
-
-
-
-    
-    housing_allowance=fields.Float("Housing Allowance",default=0.00)
-    travel_allowance=fields.Float("Travel Allowance",default=0.00)
-    increment_Date = fields.Date(string="Increment Date")
-    increment_Amount=fields.Float("Increment Amount",default=0.00)
-    leave_Status = fields.Selection([('Active', 'Active'), ('Terminated', 'Terminated'), ('Resigned', 'Resigned'),('Vacation', 'Vacation')], string='Leave Status', )
-    leave_due=fields.Float("Leave Due",default=0.00)
-    leave_amount=fields.Float("Leave Amount",default=0.00)
-    total_work_experience= fields.Char(string="Total Work Experience")
-    indemnity= fields.Char(string="Indemnity")
-    tenure= fields.Char(string="Tenure")
-    sponsorship= fields.Selection([('Design Creative', 'Design Creative'), ('Design Grafix', 'Design Grafix')], string='Sponsorship', )
-    work_location = fields.Selection([('DG', 'DG'), ('DC', 'DC')], string='Work Location', )
-
+# class inheritcontract(models.Model):
+#     _inherit = 'hr.contract'
+#
+#
+#     housing_allowance=fields.Float("Housing Allowance",default=0.00)
+#     travel_allowance=fields.Float("Travel Allowance",default=0.00)
+#     increment_Date = fields.Date(string="Increment Date")
+#     increment_Amount=fields.Float("Increment Amount",default=0.00)
+#     leave_Status = fields.Selection([('Active', 'Active'), ('Terminated', 'Terminated'), ('Resigned', 'Resigned'),('Vacation', 'Vacation')], string='Leave Status', )
+#     leave_due=fields.Float("Leave Due",default=0.00)
+#     leave_amount=fields.Float("Leave Amount",default=0.00)
+#     total_work_experience= fields.Char(string="Total Work Experience")
+#     indemnity= fields.Char(string="Indemnity")
+#     tenure= fields.Char(string="Tenure")
+#     sponsorship= fields.Selection([('Design Creative', 'Design Creative'), ('Design Grafix', 'Design Grafix')], string='Sponsorship', )
+#     work_location = fields.Selection([('DG', 'DG'), ('DC', 'DC')], string='Work Location', )
+#
 

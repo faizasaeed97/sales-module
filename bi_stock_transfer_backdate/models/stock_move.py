fbeeb9993d46175a74a43fb985e56f510edcc194
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-03-04 04:08:05 +0500

    bi

--- bi_stock_transfer_backdate/models/stock_move.py
+++ bi_stock_transfer_backdate/models/stock_move.py
@@ -28,7 +28,7 @@ class StockMoveUpdate(models.Model):
                     line.write({'date': move.transfer_date or fields.Datetime.now()})
         return res
 
-    def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id):
+    def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id,qty, description, svl_id,cost):
         self.ensure_one()
         AccountMove = self.env['account.move']
         quantity = self.env.context.get('forced_quantity', self.product_qty)
@@ -43,7 +43,9 @@ class StockMoveUpdate(models.Model):
             elif self.env.context.get('forced_quantity') is not None:
                 ref = 'Correction of %s (modification of past move)' % ref
 
-        move_lines = self.with_context(forced_ref=ref)._prepare_account_move_line(quantity, abs(self.value), credit_account_id, debit_account_id)
+        description=ref
+
+        move_lines = self.with_context(forced_ref=ref)._prepare_account_move_line(quantity, abs(self.value), credit_account_id, debit_account_id,description)
         if move_lines:
             date = self.transfer_date or self._context.get('force_period_date', fields.Date.context_today(self))
             new_account_move = AccountMove.sudo().create({
@@ -54,3 +56,24 @@ class StockMoveUpdate(models.Model):
                 'stock_move_id': self.id,
             })
             new_account_move.post()
+
+
+
+        # def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id, qty, description, svl_id,
+        #                               cost):
+        #     self.ensure_one()
+        #     AccountMove = self.env['account.move'].with_context(default_journal_id=journal_id)
+        #
+        #     move_lines = self._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id, description)
+        #     if move_lines:
+        #         date = self._context.get('force_period_date', fields.Date.context_today(self))
+        #         new_account_move = AccountMove.sudo().create({
+        #             'journal_id': journal_id,
+        #             'line_ids': move_lines,
+        #             'date': date,
+        #             'ref': description,
+        #             'stock_move_id': self.id,
+        #             'stock_valuation_layer_ids': [(6, None, [svl_id])],
+        #             'type': 'entry',
+        #         })
+        #         new_account_move.post()

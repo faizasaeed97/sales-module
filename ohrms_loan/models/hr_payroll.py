c2475d9fc6112e41ceb048dd9a97fd18a06f788e
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-05-18 12:56:12 +0500

    ohrms

--- ohrms_loan/models/hr_payroll.py
+++ ohrms_loan/models/hr_payroll.py
@@ -16,53 +16,60 @@ class HrPayslip(models.Model):
                     total += line.amount
             self.total_paid = total
 
-    @api.onchange('employee_id', 'date_from', 'date_to')
-    def onchange_employee(self):
-        if (not self.employee_id) or (not self.date_from) or (not self.date_to):
-            return
-
-        employee = self.employee_id
-        date_from = self.date_from
-        date_to = self.date_to
-
-        ttyme = datetime.fromtimestamp(time.mktime(str(time.strptime(date_from)), "%Y-%m-%d"))
-        locale = self.env.context.get('lang') or 'en_US'
-        self.name = _('Salary Slip of %s for %s') % (
-        employee.name, tools.ustr(babel.dates.format_date(date=ttyme, format='MMMM-y', locale=locale)))
-        self.company_id = employee.company_id
-
-        if not self.env.context.get('contract') or not self.contract_id:
-            contract_ids = self.get_contract(employee, date_from, date_to)
-            if not contract_ids:
-                return
-            self.contract_id = self.env['hr.contract'].browse(contract_ids[0])
-
-        if not self.contract_id.struct_id:
-            return
-        self.struct_id = self.contract_id.struct_id
-
-        # computation of the salary input
-        worked_days_line_ids = self.get_worked_day_lines(contract_ids, date_from, date_to)
-        worked_days_lines = self.worked_days_line_ids.browse([])
-        for r in worked_days_line_ids:
-            worked_days_lines += worked_days_lines.new(r)
-        self.worked_days_line_ids = worked_days_lines
-
-        input_line_ids = self.get_inputs(contract_ids, date_from, date_to)
-        input_lines = self.input_line_ids.browse([])
-        for r in input_line_ids:
-            input_lines += input_lines.new(r)
-        self.input_line_ids = input_lines
-
-        total_loan = 0
-        if self.employee_id:
-            loan_ids = self.env['hr.loan.line'].search([('employee_id', '=', self.employee_id.id), ('paid', '=', False)])
-            for loan in loan_ids:
-                total_loan = total_loan + loan.amount
-
-        self.has_outstanding = total_loan
-
-        return
+    # @api.onchange('employee_id', 'date_from', 'date_to')
+    # def onchange_employee(self):
+    #     if (not self.employee_id) or (not self.date_from) or (not self.date_to):
+    #         return
+    #
+    #     employee = self.employee_id
+    #     date_from = self.date_from
+    #     date_to = self.date_to
+    #
+    #     ttyme=datetime.strptime(str(date_from), '%Y-%m-%d')
+    #
+    #     # ttyme = datetime.fromtimestamp(str(time.mktime(str(time.strptime(date_from, "%Y-%m-%d")))))
+    #
+    #     locale = self.env.context.get('lang') or 'en_US'
+    #     self.name = _('Salary Slip of %s for %s') % (
+    #     employee.name, tools.ustr(babel.dates.format_date(date=ttyme, format='MMMM-y', locale=locale)))
+    #     self.company_id = employee.company_id
+    #
+    #     if not self.env.context.get('contract') or not self.contract_id:
+    #
+    #         # contracts = employee._get_contracts(date_from, date_to)
+    #
+    #
+    #         contract_ids = employee._get_contracts(date_from, date_to)
+    #         if not contract_ids:
+    #             return
+    #         self.contract_id = self.env['hr.contract'].browse(contract_ids[0])
+    #
+    #     # if not self.contract_id.structure_type_id:
+    #     #     return
+    #     # self.struct_id = self.contract_id.structure_type_id
+    #
+    #     # computation of the salary input
+    #     worked_days_line_ids = self._get_worked_day_lines()
+    #     worked_days_lines = self.worked_days_line_ids.browse([])
+    #     for r in worked_days_line_ids:
+    #         worked_days_lines += worked_days_lines.new(r)
+    #     self.worked_days_line_ids = worked_days_lines
+    #
+    #     input_line_ids = self.get_inputs(contract_ids, date_from, date_to)
+    #     input_lines = self.input_line_ids.browse([])
+    #     for r in input_line_ids:
+    #         input_lines += input_lines.new(r)
+    #     self.input_line_ids = input_lines
+    #
+    #     total_loan = 0
+    #     if self.employee_id:
+    #         loan_ids = self.env['hr.loan.line'].search([('employee_id', '=', self.employee_id.id), ('paid', '=', False)])
+    #         for loan in loan_ids:
+    #             total_loan = total_loan + loan.amount
+    #
+    #     self.has_outstanding = total_loan
+    #
+    #     return
 
     loan_ids = fields.One2many('hr.loan.line', 'payslip_id', string="Loans")
     total_paid = fields.Float(string="Total Loan Amount", compute='compute_total_paid')

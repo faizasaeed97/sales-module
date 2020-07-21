eff5ae3c3057b70c747956c4c582fa482a6bc370
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-07-21 07:05:19 +0500

    base

--- hr_base/models/hr.py
+++ hr_base/models/hr.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See COPYRIGHT & LICENSE files for full copyright and licensing details.
+from collections import defaultdict
 
+import pytz
 from odoo import models, fields, api, _
-import datetime
 from odoo.exceptions import UserError, ValidationError
 
+from collections import defaultdict
+from datetime import datetime, date, time, timedelta
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
 
 class HrEmployee(models.Model):
     _inherit = "hr.employee"
@@ -22,7 +27,7 @@ class HrEmployee(models.Model):
     absents = fields.Integer(string="Absents", compute="_get_days_calculation")
 
     def _get_days_calculation(self):
-        t_date_month=datetime.datetime.now().date().month
+        t_date_month=datetime.now().date().month
         for dta in self:
             recor = self.env['attendance.custom'].search(
                 [
@@ -70,15 +75,15 @@ class HrPayslip_inherits(models.Model):
                      ('employee_id', '=',
                       rec.employee_id.id)
                      ])
-                # if rec.date_to.day == 31:
-                #     if total>30:
-                #        total-=1
+                if rec.date_to.day == 31:
+                    if total>30:
+                       total-=1
 
                 df = rec.date_from
                 dt = rec.date_to
                 absnt=0
 
-                delta = datetime.timedelta(days=1)
+                delta = timedelta(days=1)
                 while df <= dt:
                     print(df)
                     check_attendence = self.env['attendance.custom'].search(
@@ -87,8 +92,6 @@ class HrPayslip_inherits(models.Model):
                     if check_attendence:
                         absnt+=1
                     df += delta
-
-
 
                 rec.total_work_day= total
                 rec.absents = absnt
@@ -108,12 +111,13 @@ class HrPayslip_inherits(models.Model):
         for payslip in self.filtered(lambda slip: slip.state not in ['cancel', 'done']):
             df = payslip.date_from
             dt = payslip.date_to
-            delta = datetime.timedelta(days=1)
+            delta = timedelta(days=1)
             while df <= dt:
                 print(df)
                 check_attendence = self.env['attendance.custom'].search(
                     [('attendance_date', '=', df), ('employee_id', '=', payslip.employee_id.id)])
                 if not check_attendence:
+                    
                     raise UserError(
                         _("Attendance of %s not found in system . Please mark attendance first") % (str(df)))
                 df += delta
@@ -153,4 +157,78 @@ class HrPayslipEmployees_inheritx(models.TransientModel):
 
         return emp
 
+    def _check_undefined_slots(self, work_entries, payslip_run):
+        """
+        Check if a time slot in the contract's calendar is not covered by a work entry
+        """
+        work_entries_by_contract = defaultdict(lambda: self.env['hr.work.entry'])
+        for work_entry in work_entries:
+            work_entries_by_contract[work_entry.contract_id] |= work_entry
 
+        for contract, work_entries in work_entries_by_contract.items():
+            calendar_start = pytz.utc.localize(datetime.combine(max(contract.date_start, payslip_run.date_start), time.min))
+            calendar_end = pytz.utc.localize(datetime.combine(min(contract.date_end or date.max, payslip_run.date_end), time.max))
+            outside = contract.resource_calendar_id._attendance_intervals(calendar_start, calendar_end) - work_entries._to_intervals()
+            if outside:
+                pass
+                # raise UserError(_("Some part of %s's calendar is not covered by any work entry. Please complete the schedule.") % contract.employee_id.name)
+
+
+
+    def compute_sheet(self):
+        self.ensure_one()
+        if not self.env.context.get('active_id'):
+            from_date = fields.Date.to_date(self.env.context.get('default_date_start'))
+            end_date = fields.Date.to_date(self.env.context.get('default_date_end'))
+            payslip_run = self.env['hr.payslip.run'].create({
+                'name': from_date.strftime('%B %Y'),
+                'date_start': from_date,
+                'date_end': end_date,
+            })
+        else:
+            payslip_run = self.env['hr.payslip.run'].browse(self.env.context.get('active_id'))
+
+        if not self.employee_ids:
+            raise UserError(_("You must select employee(s) to generate payslip(s)."))
+
+        payslips = self.env['hr.payslip']
+        Payslip = self.env['hr.payslip']
+
+        contracts = self.employee_ids._get_contracts(payslip_run.date_start, payslip_run.date_end, states=['open', 'close'])
+        contracts._generate_work_entries(payslip_run.date_start, payslip_run.date_end)
+        work_entries = self.env['hr.work.entry'].search([
+            ('date_start', '<=', payslip_run.date_end),
+            ('date_stop', '>=', payslip_run.date_start),
+            ('employee_id', 'in', self.employee_ids.ids),
+        ])
+        self._check_undefined_slots(work_entries, payslip_run)
+
+        validated = work_entries.action_validate()
+        if not validated:
+            pass
+            # raise UserError(_("Some work entries could not be validated."))
+
+        default_values = Payslip.default_get(Payslip.fields_get())
+        for contract in contracts:
+            values = dict(default_values, **{
+                'employee_id': contract.employee_id.id,
+                'credit_note': payslip_run.credit_note,
+                'payslip_run_id': payslip_run.id,
+                'date_from': payslip_run.date_start,
+                'date_to': payslip_run.date_end,
+                'contract_id': contract.id,
+                'struct_id': self.structure_id.id or contract.structure_type_id.default_struct_id.id,
+            })
+            payslip = self.env['hr.payslip'].new(values)
+            payslip._onchange_employee()
+            values = payslip._convert_to_write(payslip._cache)
+            payslips += Payslip.create(values)
+        payslips.compute_sheet()
+        payslip_run.state = 'verify'
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'hr.payslip.run',
+            'views': [[False, 'form']],
+            'res_id': payslip_run.id,
+        }

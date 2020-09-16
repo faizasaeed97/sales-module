19f050f2442a308ce4978705bc18c97b27dd7fbf
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-09-16 17:08:04 +0500

    attendance custom

---
+++ attendance_customization/models/hr_leave.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+import pytz
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+from collections import defaultdict
+from datetime import datetime, date, time, timedelta
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+
+class hr_calendar_leave(models.Model):
+    _name = 'hr.calendar.leave'
+
+    name_second = fields.Char(String="Name")
+    leave = fields.One2many('hr.leave.lineitem', 'new_id_second', String="Leave")
+
+
+class line_item_two(models.Model):
+    _name = 'hr.leave.lineitem'
+
+    new_id_second = fields.Many2one('hr.calendar.leave', string="Type ID")
+    leave_name = fields.Char( string="Leave name")
+    leave_date = fields.Date(string="Date")

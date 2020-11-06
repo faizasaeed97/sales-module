7dbfe9307e0fbfc274254b5092f576e392517789
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-11-06 18:54:48 +0500

    customize

--- attendance_customization/models/import_data.py
+++ attendance_customization/models/import_data.py
@@ -20,13 +20,14 @@ class ImportAttendance(models.TransientModel):
     def get_time(self, x):
         if x:
             x = int(x * 24 * 3600)  # convert to number of seconds
-            minute=x/60
-            hour=minute/60
+            minute = x / 60
+            hour = minute / 60
             my_time = time(x // 3600, (x % 3600) // 60, x % 60)  # hours, minutes, seconds
             print(type(my_time))
-            return str(my_time.hour)+':'+str(my_time.minute)
-
-
+            if my_time.minute == 0:
+                return str(my_time.hour) + ':' + str(my_time.minute)+"0"
+            else:
+                return str(my_time.hour) + ':' + str(my_time.minute)
 
     def is_attendance_exist_sameday(self, employee_id, attendance_date):
         return self.env['attendance.custom'].search(
@@ -43,13 +44,13 @@ class ImportAttendance(models.TransientModel):
             for row in range(1, sheet.nrows):
                 attendance_dict = {}
                 id_no = int(sheet.cell(row, 0).value)
-                emp=self.env['hr.employee'].search([('identification_id','=',id_no)])
+                emp = self.env['hr.employee'].search([('identification_id', '=', id_no)])
                 attendance_date = sheet.cell(row, 1).value
                 first_in = sheet.cell(row, 7).value
                 first_out = sheet.cell(row, 8).value
                 second_in = sheet.cell(row, 12).value
                 second_out = sheet.cell(row, 13).value
-                exception_approved= sheet.cell(row, 26).value
+                exception_approved = sheet.cell(row, 26).value
                 attendance_dict['employee_id'] = emp.id
                 attendance_dict['attendance_date'] = attendance_date
                 attendance_dict['first_in'] = self.get_time(first_in)
@@ -63,10 +64,12 @@ class ImportAttendance(models.TransientModel):
                 # create attendance records for each one, check if attendance not found on same date, else give error already exist.
                 for obj in attendance_list:
                     if obj['employee_id'] and obj['attendance_date']:
-                        print('yes..',obj['employee_id'],obj['attendance_date'])
+                        print('yes..', obj['employee_id'], obj['attendance_date'])
                         self.env['attendance.custom'].create({'employee_id': obj['employee_id'],
-                                                              'attendance_date':datetime.datetime.strptime(str(obj['attendance_date']), '%d/%m/%Y'),
+                                                              'attendance_date': datetime.datetime.strptime(
+                                                                  str(obj['attendance_date']), '%d/%m/%Y'),
                                                               'first_check_in': obj['first_in'],
                                                               'first_check_out': obj['first_out'],
                                                               'second_check_in': obj['second_in'],
-                                                              'second_check_out': obj['second_out'],'exception_approved':obj['exception_approved']})
+                                                              'second_check_out': obj['second_out'],
+                                                              'exception_approved': obj['exception_approved']})

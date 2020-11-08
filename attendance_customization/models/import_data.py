1af5911388810bef98a53700e86fb4ddf7922077
rao.kashif8787@gmail.com <rao.kashif8787@gmail.com>
2020-11-09 02:01:56 +0500

    cahne

--- attendance_customization/models/import_data.py
+++ attendance_customization/models/import_data.py
@@ -54,8 +54,8 @@ class ImportAttendance(models.TransientModel):
                         str(emp.name) + " with ID "+str(id_no) +" Does not exist as an employee")
                 else:
                     attendance_date = sheet.cell(row, 1).value
-                    # attendance_date=datetime.datetime.strptime(str(attendance_date), "%d/%m/%Y")
-                    if self.is_attendance_exist_sameday(emp.id,attendance_date.replace('/', '-')):
+                    attendance_date=datetime.datetime.strptime(str(attendance_date.replace('/', '-')), "%d-%m-%Y")
+                    if self.is_attendance_exist_sameday(emp.id,attendance_date):
                         raise UserError(str(emp.name)+ " Attendance of date "+str(attendance_date)+" is already marked.")
                     first_in = sheet.cell(row, 7).value
                     first_out = sheet.cell(row, 8).value
